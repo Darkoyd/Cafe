9bed689ad7f7a0db0c099795f9aa20480a08c6e7
Lele <nilocuru@hotmail.com>
2020-01-13 13:08:14 -0500

    Finished SuperCafé

--- Café/src/mundo/SuperCafe.java
+++ Café/src/mundo/SuperCafe.java
@@ -1,13 +1,14 @@
 package mundo;
 
-import java.util.Date;
+import java.sql.Date;
 
 public class SuperCafe {
 	
 	public enum movimiento {
 		PERGAMINO,
 		TRILLANDO,
-		EN_INVENTARIO
+		EN_INVENTARIO,
+		VENTA
 	}
 	
 	private float peso;
@@ -18,8 +19,34 @@ public class SuperCafe {
 	
 	private Date fechaLote;
 	
-	public SuperCafe() {
-		
+	private Date fechaVenta;
+	
+	public SuperCafe(float peso, Date inicial) {
+		this.peso = peso;
+		fechaPergamino = inicial;
 	}
 
+	public float getPeso() {
+		return peso;
+	}
+
+	public Date getFechaPergamino() {
+		return fechaPergamino;
+	}
+
+	public Date getFechaTrillado() {
+		return fechaTrillado;
+	}
+
+	public Date getFechaLote() {
+		return fechaLote;
+	}
+
+	public Date getFechaVenta() {
+		return fechaVenta;
+	}
+	
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
 }
